912a5fa89075038a945f5d5f6ab9086792c00c14
GiorgosChristodoulou <gatepili@hotmail.com>
2018-04-13 16:54:35 +0300

    d
    d

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,358 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+
+				<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+				<link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+				<link rel="stylesheet" href="vendor/animate/animate.min.css">
+				<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+				<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+				<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+				<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="css/default.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="js/modernizr.min.js"></script>
+
+	</head>
+	<body>
+
+
+
+			<div role="main" class="main">
+
+				<section class="page-header">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3">
+									<h1>About Me</h1>
+							</div>
+							<div class="col-md-4">
+								<ul class="social-icons">
+									<li class="social-icons-linkedin"><a href="https://www.linkedin.com/in/george-ch/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+									<li class="social-icons-github"><a href="https://github.com/GiorgosChristodoulou" target="_blank" title="GitHub"><i class="fa fa-github"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="owl-carousel owl-theme" data-plugin-options="{'items': 1, 'margin': 10}">
+								<div>
+									<span class="img-thumbnail">
+										<img src="rsz_img_0225.jpg">
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8">
+
+							<h2 class="mb-none"><strong>George Christodoulou</strong></h2>
+							<h4 class="heading-primary">Postgraduate Student at University of Ioannina</h4>
+
+							<hr class="solid">
+
+							<p>I am George Christodoulou and i was born in Kos, Greece. I received my diploma in Computer Science and Engineering from the <a href="http://www.cs.uoi.gr/"> Computer Science and Engineering Department</a> of the <a href="http://www.uoi.gr/"> University of Ioannina</a> in 2017 . In 2016 i worked as a web development intern at Natech S.A. My research interests include operating systems and routing in multiple types of networks. My interests also include web development and game development. Currently i am pursuing my MSc degree from the <a href="http://www.cs.uoi.gr/"> Computer Science and Engineering Department</a> of the <a href="http://www.uoi.gr/"> University of Ioannina.</a></p>
+
+							<ul class="list list-icons">
+								<li><i class="fa fa-file-text"></i> <a href="cv.pdf"> <strong> My Resume </strong> </a></li>
+							</ul>
+
+						</div>
+					</div>
+
+							<hr>
+
+			<section id="skills" class="section section-no-border background-color-light m-none">
+					<div class="container">
+										<div class="row ">
+											<h2 class="text-color-quaternary text-uppercase font-weight-extra-bold">Skills & Language</h2>
+											<div class="col-md-4 col-sm-6 col-xs-12">
+												<div class="progress-bars custom-progress-bars">
+													<div class="progress-label text-color-dark font-weight-semibold text-uppercase text-sm">
+														<span>Object Oriented Programming</span>
+													</div>
+													<div class="progress">
+														<div class="progress-bar" data-appear-progress-animation="100%"></div>
+													</div>
+													<div class="progress-label text-color-dark font-weight-semibold text-uppercase text-sm">
+														<span>Web Development</span>
+													</div>
+													<div class="progress">
+														<div class="progress-bar" data-appear-progress-animation="100%" data-appear-animation-delay="300"></div>
+													</div>
+													<div class="progress-label text-color-dark font-weight-semibold text-uppercase text-sm">
+														<span>Game Development</span>
+													</div>
+													<div class="progress">
+														<div class="progress-bar" data-appear-progress-animation="100%" data-appear-animation-delay="600"></div>
+													</div>
+													<div class="progress-label text-color-dark font-weight-semibold text-uppercase text-sm">
+														<span>Networks</span>
+													</div>
+													<div class="progress">
+														<div class="progress-bar" data-appear-progress-animation="100%" data-appear-animation-delay="900"></div>
+													</div>
+												</div>
+											</div>
+
+											<div class="col-md-4 col-sm-6 col-xs-12">
+												<div class="custom-box-details background-color-light custom-box-shadow-1">
+													<h4 class="text-color-dark">Languages</h4>
+													<ul class="custom-list-style-1 p-none">
+														<li>
+															<span class="font-weight-semibold custom-max-width-1 text-color-dark">Greek:</span>
+															<span class="custom-text-color-2">Native</span>
+														</li>
+														<li>
+															<span class="font-weight-semibold custom-max-width-1 text-color-dark">Dutch:</span>
+															<span class="custom-text-color-2">Native</span>
+														</li>
+														<li>
+															<span class="font-weight-semibold custom-max-width-1 text-color-dark">English:</span>
+															<span class="custom-text-color-2">Advanced</span>
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</div>
+								</section>
+
+								<hr>
+
+				<section class="parallax section section-parallax section-center" data-plugin-parallax data-plugin-options="{'speed': 1.5}" data-image-src="img/parallax-7.jpg">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="owl-carousel owl-theme nav-bottom rounded-nav mt-lg mb-none" data-plugin-options="{'items': 1, 'loop': false}">
+									<div>
+										<div class="col-md-12">
+											<div class="testimonial testimonial-style-6 testimonial-with-quotes mb-none">
+												<blockquote>
+													<p>Theory is when you know everything and nothing works. Practice is when everything works but no one knows why.  In our case, theory and practice are combined: nothing works and no one knows why.</p>
+												</blockquote>
+												<div class="testimonial-author">
+													<p><strong>Unknown</strong></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<hr>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+
+							<h4 class="mt-xlg mb-none text-uppercase"><strong>My Work</strong></h4>
+							<p>Here are some of the projects i have worked on until now:</p>
+
+							<div class="row">
+
+								<ul class="portfolio-list">
+									<div class="owl-carousel owl-theme" data-margin="50" data-autoplay="true">
+									<li class="col-md-12">
+										<div class="portfolio-item">
+											<a href="gradOSproject.rar">
+												<span class="thumb-info thumb-info-lighten">
+													<span class="thumb-info-wrapper">
+														<img src="sgx.jpg" class="img-responsive" alt="">
+														<span class="thumb-info-title">
+															<span class="thumb-info-inner">SGX-enabled Tor</span>
+															<span class="thumb-info-type">MSc Course project</span>
+														</span>
+														<span class="thumb-info-action">
+															<span class="thumb-info-action-icon"><i class="fa fa-file-zip-o"></i></span>
+														</span>
+
+													</span>
+												</span>
+											</a>
+										</div>
+									</li>
+
+									<li class="col-md-12">
+										<div class="portfolio-item">
+											<a href="TFWS.zip">
+												<span class="thumb-info thumb-info-lighten">
+													<span class="thumb-info-wrapper">
+														<img src="social.jpg" class="img-responsive" alt="">
+														<span class="thumb-info-title">
+															<span class="thumb-info-inner">Teams with Status</span>
+															<span class="thumb-info-type">MSc Course project</span>
+														</span>
+														<span class="thumb-info-action">
+															<span class="thumb-info-action-icon"><i class="fa fa-file-zip-o"></i></span>
+														</span>
+													</span>
+												</span>
+											</a>
+										</div>
+									</li>
+
+									<li class="col-md-12">
+										<div class="portfolio-item">
+											<a href="diploma.rar">
+												<span class="thumb-info thumb-info-lighten">
+													<span class="thumb-info-wrapper">
+														<img src="dtn.jpg" class="img-responsive" alt="">
+														<span class="thumb-info-title">
+															<span class="thumb-info-inner"> DTN Routing Protocol</span>
+															<span class="thumb-info-type">BSc Diploma Thesis</span>
+														</span>
+														<span class="thumb-info-action">
+															<span class="thumb-info-action-icon"><i class="fa fa-file-zip-o"></i></span>
+														</span>
+													</span>
+												</span>
+											</a>
+										</div>
+									</li>
+
+									<li class="col-md-12">
+										<div class="portfolio-item">
+											<a href="2dGame.zip">
+												<span class="thumb-info thumb-info-lighten">
+													<span class="thumb-info-wrapper">
+														<img src="games.png" class="img-responsive" alt="">
+														<span class="thumb-info-title">
+															<span class="thumb-info-inner">2D Game</span>
+															<span class="thumb-info-type">BSc Course project</span>
+														</span>
+														<span class="thumb-info-action">
+															<span class="thumb-info-action-icon"><i class="fa fa-file-zip-o"></i></span>
+														</span>
+													</span>
+												</span>
+											</a>
+										</div>
+									</li>
+
+									<li class="col-md-12">
+										<div class="portfolio-item">
+											<a href="3dGame.zip">
+												<span class="thumb-info thumb-info-lighten">
+													<span class="thumb-info-wrapper">
+														<img src="games.png" class="img-responsive" alt="">
+														<span class="thumb-info-title">
+															<span class="thumb-info-inner">3D Game</span>
+															<span class="thumb-info-type">BSc Course project</span>
+														</span>
+														<span class="thumb-info-action">
+															<span class="thumb-info-action-icon"><i class="fa fa-file-zip-o"></i></span>
+														</span>
+													</span>
+												</span>
+											</a>
+										</div>
+									</li>
+
+									<li class="col-md-12">
+										<div class="portfolio-item">
+											<a href="MinusXl-master.zip">
+												<span class="thumb-info thumb-info-lighten">
+													<span class="thumb-info-wrapper">
+														<img src="spreadsheet.jpg" class="img-responsive" alt="">
+														<span class="thumb-info-title">
+															<span class="thumb-info-inner">MinusXl</span>
+															<span class="thumb-info-type">BSc Course project</span>
+														</span>
+														<span class="thumb-info-action">
+															<span class="thumb-info-action-icon"><i class="fa fa-file-zip-o"></i></span>
+														</span>
+													</span>
+												</span>
+											</a>
+										</div>
+									</li>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+
+			<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="footer-ribbon">
+							<span>Get in Touch</span>
+						</div>
+						<div class="col-md-3">
+							<div class="contact-details">
+								<h4>Contact Me</h4>
+								<ul class="contact">
+									<li><p><i class="fa fa-map-marker"></i> <strong>Address:</strong> Karaoli k Dhmhtriou 3, Ioannina, Greece</p></li>
+									<!--<li><p><i class="fa fa-phone"></i> <strong>Phone:</strong> +306973180106</p></li>-->
+									<li><p><i class="fa fa-envelope"></i> <strong>Email:</strong> <a href="mailto:mail@example.com">jorgos.christodoulou@gmail.com</a></p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<ul class="social-icons">
+								<li class="social-icons-linkedin"><a href="https://www.linkedin.com/in/george-ch/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+								<li class="social-icons-github"><a href="https://github.com/GiorgosChristodoulou" target="_blank" title="GitHub"><i class="fa fa-github"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+
+		<!-- Vendor -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.appear.min.js"></script>
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/jquery-cookie.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/common.min.js"></script>
+		<script src="js/jquery.validation.min.js"></script>
+		<script src="js/jquery.easy-pie-chart.min.js"></script>
+		<script src="js/jquery.gmap.min.js"></script>
+		<script src="js/jquery.lazyload.min.js"></script>
+		<script src="js/jquery.isotope.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/vide.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+
+	</body>
+</html>
